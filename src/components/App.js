9cdcd80764c2c10302587fcdf434a151be3adfcc
Isabela <isabela_cmoreira@hotmail.com>
2022-01-05 10:00:57 -0300

    Creation of routes

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Registering from "./Registering";
+import Habits from "./Habits";
+import Today from "./Today";
+import History from "./History";
+
+
+export default function App() {
+      
+    return (
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/cadastro" element={<Registering/>} />
+          <Route path="/habitos" element={<Habits/>}/>
+          <Route path="/hoje" element={<Today/>} />
+          <Route path="/historico" element={<History/>} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
